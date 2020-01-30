53d9204ae5f45f9dc9ab70e87f37aedcdd7c1e8b
Rob Christian <rm.rf.etc@gmail.com>
2020-01-29 22:55:26 -0800

    Remove unnecessary destructuring of color values in color-picker

--- react-app/src/app4/components/color-picker.js
+++ react-app/src/app4/components/color-picker.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { ChromePicker } from 'react-color';
 
 export default () => {
-	const [{r,g,b}, setColor] = React.useState({ r:0, g:0, b:0 });
+	const [color, setColor] = React.useState({ r:0, g:0, b:0 });
 	return (
 		<div className="col3">
 			<ChromePicker
-				color={{r,g,b}}
-				onChange={({ rgb: {r,g,b} }) => setColor({r,g,b})}
+				color={color}
+				onChange={({ rgb }) => setColor(rgb)}
 			/>
 		</div>
 	);

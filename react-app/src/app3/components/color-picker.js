c69e2859812d0b3f99765c608b08a6a52dbe9344
Rob Christian <rm.rf.etc@gmail.com>
2020-01-30 19:02:01 -0800

    Remove local state from app1 and change port to 4040

--- react-app/src/app3/components/color-picker.js
+++ react-app/src/app3/components/color-picker.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ChromePicker } from 'react-color';
 
+const color = { r:0, g:0, b:0 };
+
 export default () => {
-	const [color, setColor] = React.useState({ r:0, g:0, b:0 });
 	return (
 		<div className="col3">
 			<ChromePicker
 				color={color}
-				onChange={({ rgb }) => setColor(rgb)}
+				onChange={() => {}}
 			/>
 		</div>
 	);
